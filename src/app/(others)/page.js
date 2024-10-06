5c9775c5acd6e10409f6bfc188b4f64b69a67d0f
Sahand Ghavidel <sahand.gha@gmail.com>
2024-10-06 19:08:44 +0700

    Add input component and update the home page

--- src/app/(others)/page.js
+++ src/app/(others)/page.js
@@ -1,5 +1,12 @@
+import Input from '@/components/Input';
+
 export default function Home() {
   return (
-    <div className='text-red-500'>Home</div>
-  )
+    <div className='min-h-screen max-w-xl mx-auto border-r border-l'>
+      <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
+        <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
+      </div>
+      <Input />
+    </div>
+  );
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useUser } from '@clerk/nextjs';
+import { HiOutlinePhotograph } from 'react-icons/hi';
+
+export default function Input() {
+  const { user, isSignedIn, isLoaded } = useUser();
+
+  if (!isSignedIn || !isLoaded) {
+    return null;
+  }
+  return (
+    <div className='flex border-b border-gray-200 p-3 space-x-3 w-full'>
+      <img
+        src={user.imageUrl}
+        alt='user-img'
+        className='h-11 w-11 rounded-full cursor-pointer hover:brightness-95 object-cover'
+      />
+      <div className='w-full divide-y divide-gray-200'>
+        <textarea
+          className='w-full border-none outline-none tracking-wide min-h-[50px] text-gray-700 '
+          placeholder='Whats happening'
+          rows='2'
+        ></textarea>
+        <div className='flex items-center justify-between pt-2.5'>
+          <HiOutlinePhotograph className='h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer' />
+          <button
+            disabled
+            className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'
+          >
+            Post
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
