55976cbf0f6ee3cb9f8ce3346e7f2650c3d1a238
Sahand Ghavidel <sahand.gha@gmail.com>
2024-10-06 13:33:11 +0700

    fix the import problem

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,6 +1,7 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
 import { clerkClient } from '@clerk/nextjs/server';
+import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
